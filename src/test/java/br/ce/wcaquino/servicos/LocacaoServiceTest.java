5c67894d37a52771079a5b36e30fe89ad9bab905
Mikael Tavares <mikael.tavares@unigranrio.br>
2022-01-03 21:40:09 -0300

    Adicionando os teste de unidade de LocacaoService

---
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -0,0 +1,31 @@
+package br.ce.wcaquino.servicos;
+
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
+
+
+public class LocacaoServiceTest {
+    
+    @Test
+    public void teste(){
+
+        //cenário
+        LocacaoService locacaoService = new LocacaoService();
+        Usuario usuario = new Usuario("Mikael");
+        Filme filme = new Filme("A culpa é das estrelas", 4, 40.00);
+        //ação
+        Locacao locacao = locacaoService.alugarFilme(usuario, filme);
+
+        //verificação
+        Assert.assertTrue(locacao.getValor() == 40.00);
+        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+    }
+}
