bfeb33b2a7d30fff3694037f93e6e148df06cd37
Mikael Tavares <mikael.tavares@unigranrio.br>
2022-01-04 00:36:19 -0300

    Adicionando teste de erros

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -1,9 +1,14 @@
 package br.ce.wcaquino.servicos;
 
 import java.util.Date;
+import java.util.regex.MatchResult;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
@@ -13,19 +18,46 @@ import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
     
+
     @Test
-    public void teste(){
+    public void teste() throws Exception{
 
         //cenário
         LocacaoService locacaoService = new LocacaoService();
         Usuario usuario = new Usuario("Mikael");
-        Filme filme = new Filme("A culpa é das estrelas", 4, 40.00);
+        Filme filme = new Filme("A culpa é das estrelas", 2, 40.00);
         //ação
         Locacao locacao = locacaoService.alugarFilme(usuario, filme);
 
         //verificação
-        Assert.assertEquals(40.00, locacao.getValor(), 0.01);
+        Assert.assertTrue(locacao.getValor() == 40.00);
         Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
         Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+    }
+
+
+    @Test(expected = Exception.class)
+    public void verificaSeEstaSemOFilmeNoEstoqueFormaElegante() throws Exception{
+
+        //cenário
+        LocacaoService locacaoService = new LocacaoService();
+        Usuario usuario = new Usuario("Mikael");
+        Filme filme = new Filme("A culpa é das estrelas", 2, 40.00);
+        
+        //ação
+        Locacao locacao = locacaoService.alugarFilme(usuario, filme);
+    }
+
+    @Test(expected = Exception.class)
+    public void verificaSeEstaSemOFilmeNoEstoqueNovaForma() throws Exception{
+
+        //cenário
+        LocacaoService locacaoService = new LocacaoService();
+        Usuario usuario = new Usuario("Mikael");
+        Filme filme = new Filme("A culpa é das estrelas", 2, 40.00);
+        //ação
+        Locacao locacao = locacaoService.alugarFilme(usuario, filme);
+
+        //verificação
     }
 }
